2be5deb461acfabf1a6126661315fa9578fe7a7e
arinayashinta-rgb <arinayashinta@gmail.com>
2026-04-01 01:37:35 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -454,3 +454,27 @@ elif st.session_state.page == "anggota":
     st.markdown(html_table, unsafe_allow_html=True)
 
     st.markdown('</div>', unsafe_allow_html=True)
+
+# =========================
+# DATASET
+# =========================
+elif st.session_state.page == "dataset":
+
+    st.markdown('<div class="glass">', unsafe_allow_html=True)
+    navbar()
+
+    st.markdown("<h1 style='font-size:42px; font-weight:900;'>📂 Dataset</h1>", unsafe_allow_html=True)
+
+    st.markdown("<h3 style='font-size:28px;'>Tabel Data</h3>", unsafe_allow_html=True)
+
+    # tampilkan dataframe
+    st.dataframe(df, use_container_width=True)
+
+    # optional: info jumlah data
+    st.markdown(f"""
+    <div style='font-size:22px; font-weight:600; margin-top:20px;'>
+    Total Data: {len(df)} baris
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.markdown('</div>', unsafe_allow_html=True)
